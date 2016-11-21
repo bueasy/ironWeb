34f444fe3d971f2308221d550cf165f918f7d514
bueasy <bueasy@gmail.com>
2016-11-21 19:36:30 +0800

    html

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,6 @@ extern crate iron;
 extern crate staticfile;
 extern crate mount;
 
-// This example serves the docs from target/doc/staticfile at /doc/
-//
-// Run `cargo doc && cargo run --example doc_server`, then
-// point your browser to http://127.0.0.1:3000/doc/
-
 use std::path::Path;
 
 use iron::Iron;
